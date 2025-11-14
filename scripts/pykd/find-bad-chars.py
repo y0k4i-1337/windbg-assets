20c0d93f4396700a07c295bd2429a74e80588aa8
Mayk <7395296+y0k4i-1337@users.noreply.github.com>
2025-11-14 18:20:23 -0300

    Add pykd script find-bad-chars

---
+++ scripts/pykd/find-bad-chars.py
@@ -0,0 +1,152 @@
+import argparse
+
+import pykd
+
+
+def str_to_int(string):
+    return int(string, 16)
+
+
+def csvs_to_int_list(csvs):
+    return [str_to_int(x) for x in csvs.split(",")]
+
+
+class BadCharFinder:
+
+    def __init__(self, addr, start, end, bad):
+        try:
+            self.addr = pykd.expr(addr)
+        except:
+            self.addr = int(addr, 16)
+        self.start = start
+        self.end = end
+        self.bad = bad
+        self.new_bad = list()
+        self.expected = list()
+        self.results = list()
+
+    def create_expected(self):
+        self.expected = [
+            i for i in range(self.start, self.end + 1) if i not in self.bad
+        ]
+
+    def compare(self):
+        prev_bad = False
+        for i in range(len(self.expected)):
+            mem = pykd.loadBytes(self.addr + i, 1)[0]
+            if mem == self.expected[i]:
+                prev_bad = False
+                continue
+            if not prev_bad:
+                self.new_bad.append(self.expected[i])
+                prev_bad = True
+                continue
+            print(
+                "[+] Consecutive bad chars (data possibly truncated), aborting..."
+            )
+            break
+
+    def find(self):
+        self.create_expected()
+        self.compare()
+
+    def __str__(self):
+        if not self.new_bad:
+            return "[+] No bad characters found"
+        else:
+            chars = ",".join(["0x{:02x}".format(x) for x in self.new_bad])
+            return "[+] Bad chars: {}".format(chars) + "\n"
+
+
+def find_bad_chars(args):
+
+    finder = BadCharFinder(args.address, args.start, args.end, args.bad)
+    finder.find()
+    print(finder)
+
+
+def generate_byte_string(args):
+    known_bad = ", ".join(f"{x:02x}" for x in args.bad)
+    var_str = f"chars = bytes(i for i in range({args.start}, {args.end + 1}) if i not in [{known_bad}])"
+
+    print("[+] characters as a range of bytes")
+    print(var_str, end="\n\n")
+
+    print("[+] characters as a byte string")
+
+    # deliberately not using enumerate since it may not execute in certain situations depending on user input for the
+    # range bounds
+    counter = args.start
+    emptyLine = True
+
+    print("badchars  = b'", end="")
+
+    for i in range(args.start, args.end + 1):
+        if counter % 16 == 0 and not emptyLine:
+            # start a new line
+            print("'")
+            print("badchars += b'", end="")
+            emptyLine = True
+
+        counter += 1
+
+        if i in args.bad:
+            continue
+        else:
+            print(f"\\x{i:02x}", end="")
+            emptyLine = False
+
+    if counter != 0 and not emptyLine:
+        print("'")
+
+
+def main():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-s",
+        "--start",
+        help="first byte in range to search (default: 00)",
+        default="00",
+        type=str_to_int,
+    )
+    parser.add_argument(
+        "-e",
+        "--end",
+        help="last byte in range to search (default: ff)",
+        default="ff",
+        type=str_to_int,
+    )
+    parser.add_argument(
+        "-b",
+        "--bad",
+        help="known bad characters (ex: `-b 00,0a,0d`)",
+        default=[],
+        type=csvs_to_int_list,
+    )
+    mutuals = parser.add_mutually_exclusive_group(required=True)
+    mutuals.add_argument(
+        "-a",
+        "--address",
+        help="address from which to begin character comparison",
+    )
+    mutuals.add_argument(
+        "-g",
+        "--generate",
+        help="generate a byte string suitable for use in source code",
+        action="store_true",
+    )
+    args = parser.parse_args()
+
+    if args.start > args.end:
+        print("[-] Start byte cannot be greater than end byte")
+        return
+
+    if args.address is not None:
+        find_bad_chars(args)
+    else:
+        generate_byte_string(args)
+
+
+if __name__ == "__main__":
+    main()
